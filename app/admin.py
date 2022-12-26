3116fbcac1a1ce74e735515156f3ca2bf2d10061
andres <andred.mopez@gmail.com>
2022-12-26 18:09:57 -0300

    added type of file in admin views and created user profiles whit comments and articles

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Article, Comment, File
+from .models import Article, Comment, File, TypeOfFile
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ from .models import Article, Comment, File
 admin.site.register(Article)
 admin.site.register(Comment)
 admin.site.register(File)
+admin.site.register(TypeOfFile)
