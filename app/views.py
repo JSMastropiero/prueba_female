eb8c9d6119992737682f6bff376b3b27c64fc049
andres <andred.mopez@gmail.com>
2022-12-27 01:33:26 -0300

    dynamic token

--- app/views.py
+++ app/views.py
@@ -32,9 +32,27 @@ class Login(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         login_serializer = self.serializer_class(data=request.data, context = {'request':request})
         if login_serializer.is_valid():
-            print(login_serializer.validated_data['user'])
+            user = login_serializer.validated_data['user']
+            if user.is_active:
+                token,created = Token.objects.get_or_create(user = user)
+                if created:
+                    return Response({
+                        'token':token.key,
+                        'message': 'token created'
+                    })
+                else:
+                    token.delete()
+                    token = Token.objects.create(user = user)
+                    return Response({
+                        'token':token.key,
+                        'message': 'token created'
+                    })
+            else:
+                print('user not activate') 
+                return Response({'error': 'User not activate'})
         else: 
-            print('no paso')
+            print('not valid')
+            return Response({'error': 'username or password invalid'})
         return Response({'mensaje': 'mensaje desde response'})
 
 
